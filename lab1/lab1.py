57a676e0943508bbcce07c2f1881c18186951d09
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-10-29 18:20:46 +0100

    1. labos: Nvm

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -12,17 +12,8 @@ n = int(input("Unesite redni broj n trazenog clana niza: "))
 
 ''' 1. Rjesavanje pomocu formule za opce rjesenje homogene rekurzivne relacije '''
 
-# Ako su nultocke iste, koeficijent se mnozi s n
-def mul(x, r):
-    if x == 0:
-        return 1
-    if x == 1:
-        return r if nul[0] == nul[1] else 1
-    if x == 2:
-        return (r if nul[0] == nul[2] else 1) * (r if nul[1] == nul[2] else 1)
-
 # Pronaci lambda multiplikatore (x,y,z): rijesi sustav 3 jednadzbe s 3 nepoznanice
-sys = [[mul(i, j) * (nul[i] ** j) for i in range(3)] + [cache[j]] for j in range(3)]
+sys = [[nul[i] ** j for i in range(3)] + [cache[j]] for j in range(3)]
 
 afbe = sys[0][0] * sys[1][1] - sys[0][1] * sys[1][0]
 ajbi = sys[0][0] * sys[2][1] - sys[0][1] * sys[2][0]
@@ -35,7 +26,7 @@ rj[1] = (ahde + ceag * rj[2]) / afbe
 rj[0] = (sys[0][3] - sys[0][1] * rj[1] - sys[0][2] * rj[2]) / sys[0][0]
 
 # Formula opceg rjesenja
-a = int(sum([mul(i, n) * rj[i] * (nul[i] ** n) for i in range(3)]))
+a = int(sum([rj[i] * (nul[i] ** n) for i in range(3)]))
 
 print("Vrijednost n-tog clana niza pomocu formule: " + str(a))
 
